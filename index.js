ae0e0a3a4b8a89f3992ba4b68d0a5733e7e47cb0
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-12 19:50:22 +0300

    edit cors and URLs

--- index.js
+++ index.js
@@ -10,46 +10,21 @@ require("dotenv").config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
-// app.options("/", (req, res) => {
-//     res.setHeader("Access-Control-Allow-Origin", "https://real-estate-mern-full-stack-ui.vercel.app");
-//     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//     res.sendStatus(204);
-//   });
-
 app.use(bodyParser.json());
-// app.use(cors({origin: 'https://real-estate-mern-full-stack-ui.vercel.app' }));
-// app.use((req, res, next) => {
-// //   res.setHeader('Access-Control-Allow-Credentials', true)
-// //   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader('Access-Control-Allow-Origin',origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   )
-//     next();
-// });
 
-// const originAllowed = 'https://real-estate-mern-full-stack-ui.vercel.app';
-// app.use(cors({
-//     origin: originAllowed,
-//     methods: ['HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-//     credentials: true,
-//   }));
+const originAllowed = 'https://real-estate-mern-full-stack-ui.vercel.app';
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Credentials', true)
-//     res.header('Access-Control-Allow-Origin', 'https://real-estate-mern-full-stack-ui.vercel.app')
-//     res.header('Access-Control-Allow-Methods','HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'DELETE')
-//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization')
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//       )
-//     next()
-// });
+app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Origin', originAllowed)
+    res.header('Access-Control-Allow-Methods','HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'DELETE')
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      )
+    next()
+});
 app.use([
     compression(),
     express.json(),
@@ -60,41 +35,9 @@ app.use(morgan('dev'));
 if (process.env.NODE_ENV === 'development') {
 
 };
-// const handlePreflight = (req, res, next) => {
-//     if (req.method === 'OPTIONS') {
-//         // Respond to preflight requests with a 200 status
-//         res.status(200).end();
-//     } else {
-//         next();
-//     }
-// };
-// const allowCors = fn => async (req, res) => {
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     // another common pattern
-//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//     )
-//     if (req.method === 'OPTIONS') {
-//       res.status(200).end()
-//       return
-//     }
-//     return await fn(req, res)
-//   }
-  
-//   const handler = (req, res) => {
-//     const d = new Date()
-//     res.end(d.toString())
-//   }
-// app.use(allowCors(handler));
-app.use('/', router)
-// app.get('/', (req, res) => {
-//     console.log('check test route')
-//     res.send('check test route response')
-// })
+
+app.use('/api/v1', router)
+
 const mongooseUrl = process.env.Mongoose_Url;
 mongoose.connect(mongooseUrl, {
     useNewUrlParser: true,
