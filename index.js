1a072608a8654ab718a407f086d70e72f5e23c69
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-13 17:40:24 +0300

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const originAllowed = 'https://real-estate-mern-full-stack-ui.vercel.app';
+const originAllowed = '*';
 
 app.use((req, res, next) => {
     // res.header('Access-Control-Allow-Credentials', true)
