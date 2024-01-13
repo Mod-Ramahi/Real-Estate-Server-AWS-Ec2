828269162d89cedac8db8314659f6890ab021f28
Mod-Ramah <m.ramahi989@gmail.com>
2024-01-13 19:39:17 +0300

    first modifying

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
-    res.header('Content-Security-Policy', 'upgrade-insecure-requests')
+    // res.header('Content-Security-Policy', 'upgrade-insecure-requests')
     next()
 });
 app.use([
